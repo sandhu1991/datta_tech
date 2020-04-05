c467e1bf3395ef5e9eb4a741a8b58b29d018ea31
hardilpreet <hardilpreet.singh@workland.com>
2020-04-05 18:01:51 -0400

    changes

--- routes/index.js
+++ routes/index.js
@@ -32,6 +32,11 @@ router.post('/submit', function(req, res, next) {
   var content = 'Name: '+ req.body.lastname+', '+ req.body.firstname + '\n';
   content+= 'Phone: ' + req.body.phone + '\n' + 'Email: ' + req.body.email + '\n';
   content+= 'Msg :' + req.body.text;
+
+  var replayContent = "Dear Customer,\n\n";
+  replayContent += "Thank you for reaching out to DATTA Tech Consulting support desk. We have received your email, and our support team will be in touch with you soon.\n\n";
+  replayContent += " Please note that our working hours is 0900 to 1700 from Monday to Saturday. \n We regret the delay in reply over the non-working hours. \n\n";
+  replayContent += "Thank you for your understanding.\n Regards,\n Support Team \n DATTA TECH CONSULTING \n Tel: +1 613 501 8777";
   
   var transporter = nodemailer.createTransport({
     service: 'gmail',
@@ -42,7 +47,7 @@ router.post('/submit', function(req, res, next) {
   });
   var mailOptionsCompany = {
     from: 'dattatechconsulting@gmail.com',
-    // to: 'sandhu.hardilpreet@gmail.com',
+    //  to: 'sandhu.hardilpreet@gmail.com',
      to: 'info@dattatechconsulting.com',
     subject: req.body.subject,
     text: content
@@ -50,24 +55,24 @@ router.post('/submit', function(req, res, next) {
   var mailOptionsUser = {
     from: 'dattatechconsulting@gmail.com',
     to: req.body.email,
-    subject: "Dont reply to this mail",
-    text: "We will help you sson"
+    subject: req.body.subject,
+    text: replayContent
   };
-  // transporter.sendMail(mailOptionsCompany, function(error, info){
-  //   if (error) {
-  //     console.log(error);
-  //   } else {
-  //     console.log('Email sent to company: ' + info.response);
-  //     transporter.sendMail(mailOptionsUser, function(error, info){
-  //       if (error) {
-  //         console.log(error);
-  //       } else {
-  //         console.log('Email sent to user: ' + info.response);
-  //       }
-  //     })
-  //     res.render('submit', {page:'Contact Us', menuId:'contact', response:"sucess", msg: "Thanks for the message! We‘ll be in touch :)"});
-  //   }
-  // });
+  transporter.sendMail(mailOptionsCompany, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent to company: ' + info.response);
+      transporter.sendMail(mailOptionsUser, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent to user: ' + info.response);
+        }
+      })
+      res.render('submit', {page:'Contact Us', menuId:'contact', response:"sucess", msg: "Thanks for the message! We‘ll be in touch :)"});
+    }
+  });
 });
 
 router.get('/jobs', function(req, res, next) {
